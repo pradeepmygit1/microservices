73191b817bcfc768b910a70e5945d1ef24cc6f71
pradeepmygit1 <pradeepmygit1@gmail.com>
2023-09-27 19:55:32 +0530

    update on AccountsController-createAccount(..) with new method, CustomerRepository-findByMobileNumber , AccountsServiceImpl-createAccount(..) and new method createNewAccount(..)

--- section2/accounts/src/main/java/com/eazybytes/accounts/controller/AccountsController.java
+++ section2/accounts/src/main/java/com/eazybytes/accounts/controller/AccountsController.java
@@ -1,8 +1,10 @@
 package com.eazybytes.accounts.controller;
 
 import com.eazybytes.accounts.constants.AccountsConstants;
-import com.eazybytes.accounts.dto.AccountsDto;
+import com.eazybytes.accounts.dto.CustomerDto;
 import com.eazybytes.accounts.dto.ResponseDto;
+import com.eazybytes.accounts.service.IAccountsService;
+import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -14,10 +16,14 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping(path = "/api",produces = MediaType.APPLICATION_JSON_VALUE)
+@AllArgsConstructor
 public class AccountsController {
 
+    private IAccountsService iAccountsService;
+
     @PostMapping("/create")
-    public ResponseEntity<ResponseDto> createAccount(@RequestBody AccountsDto accountsDto){
+    public ResponseEntity<ResponseDto> createAccount(@RequestBody CustomerDto customerDto){
+        iAccountsService.createAccount(customerDto);
          return ResponseEntity
                  .status(HttpStatus.CREATED)
                  .body(new ResponseDto(AccountsConstants.STATUS_201,AccountsConstants.MESSAGE_201));
